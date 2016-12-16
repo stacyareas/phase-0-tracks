d61bea2712c012020a1ba33b73eb2055344d1bbc
Stacy Areas <stacyareas@gmail.com>
2016-12-16 15:51:06 -0800

    added a final nested data structure and printed certain results

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,24 +1,48 @@
 burgers = {
-		cheese_burger: {
-			chesse_option_1: 'cheddar',
-			cheese_option_2: 'jack',
-			cheese_option_3: 'blue cheese',
+		veggie_burger: {
+			cheese_opt_1: 'pepper jack',
+			cheese_opt_2: 'gouda',
+			cheese_opt_3: 'swiss',
 			bun: {
-				option_1: 'gluten free',	
-	},
-	meat_temp: [
-		'rare', 'med', 'well'
-	]
+				opt_1: 'gluten free',
+				opt_2: 'no bun'	
 },
-	veggie_burger: {
-			cheese_option_1: 'pepper jack',
-			cheese_option_2: 'gouda',
-			cheese_option_3: 'swiss',
+		meat_temp: [
+		'rare', 'med', 'well'
+		]
+},
+		cheese_burger: {
+			chesse_opt_1: 'cheddar',
+			cheese_opt_2: 'jack',
+			cheese_opt_3: 'blue cheese',
 			bun: {
-				option_1: 'gluten free',
-				option_2: 'no bun',
-			}	
-		},
-	}
-	
- 
+				opt_1: 'gluten free',
+				opt_2: 'no bun'	
+	},
+		meat_temp: [
+		'rare', 'med', 'well'
+		]
+},
+		special_burger: {
+			topping_op_1: 'bacon',
+			topping_op_2: 'egg',
+			topping_op_3: 'avocado',
+			type_of_meat: {
+				opt_1: 'lamb',
+				opt_2: 'bison',	
+				opt_3: 'beef'
+	},
+		meat_temp: [
+		'rare', 'med', 'well'
+		]	
+ 	} 
+  }  
+
+p burgers[:cheese_burger][:meat_temp][2]
+
+p burgers[:veggie_burger][:cheese_opt_2].upcase
+
+p burgers[:special_burger][:type_of_meat][:opt_2]
+
+p burgers[:cheese_burger][:meat_temp]
+
