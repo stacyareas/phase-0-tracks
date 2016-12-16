d5a87f29fc7fd33a5d597a3c7d2f9d97cba4eefa
Stacy Areas <stacyareas@gmail.com>
2016-12-16 14:46:42 -0800

    added 2 nested data structures for types of burgers

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,24 @@
+burgers = {
+		cheese_burger: {
+			chesse_option_1: 'cheddar',
+			cheese_option_2: 'jack',
+			cheese_option_3: 'blue cheese',
+			bun: {
+				option_1: 'gluten free',	
+	},
+	meat_temp: [
+		'rare', 'med', 'well'
+	]
+},
+	veggie_burger: {
+			cheese_option_1: 'pepper jack',
+			cheese_option_2: 'gouda',
+			cheese_option_3: 'swiss',
+			bun: {
+				option_1: 'gluten free',
+				option_2: 'no bun',
+			}	
+		},
+	}
+	
+ 
