80693ffb70b27a9a173d811f81055d53266b0714
Stacy Areas <stacyareas@gmail.com>
2016-12-15 17:27:23 -0800

    added option to edit user value and prints new results

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -17,7 +17,25 @@ clients [:age] = age
 clients [:kids] = kids
 clients [:decor_color] = decor_color
 
-p clients 
- 
+p clients
+
+puts "Would you like to update any of your information?"
+answer = gets.chomp
+
+if answer == "yes"
+	puts "Which one would you like to update?"
+    update = gets.chomp
+    puts "What would you like to change it to?"
+	change = gets.chomp
+	clients [:"#{update}"] = change
+else 
+	"exit"
+end
+
+
+p clients
+
+
+
 
 
