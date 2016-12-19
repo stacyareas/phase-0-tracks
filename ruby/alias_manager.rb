a8b6dd0d420ac70e41810c8f09a0e064673a867b
Stacy Areas <stacyareas@gmail.com>
2016-12-19 00:21:12 -0800

    created a loop to take in multi. names and prints results

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,29 +1,39 @@
-user_names = {}
-
+#Create a method that takes each vowel and switches it out for the next vowel.
 def fake_name(name)
 	vowels = ["a", "e", "i", "o", "u"]
   name = name.chars
   new_name = name.map do |char|
+
+ # If the character is a vowel is will rotate it for the next vowel in line. 	
     if vowels.include?(char) 
       vowels.rotate(1)[vowels.index(char)]
+
+# Otherwise if the character isn't a vowel it should switch to the next letter
+# in the alphabet.     
     else
-      char
+      char.next
     end
   end
-  new_name.join.split(' ').reverse
+# New name characters show be join together and re-spilt to two spereate strings.  
+  new_name.join.split(' ')
 end
 
-puts "what is your name?"
-name = gets.chomp
+# Then print results
+p fake_name("stacy areas")
 
-user_names [:name] = name
-user_names [:new_name] = fake_name
+# Create an empty array so that the users names can be colleted into
+# one variable.
+names = []
 
-while true
-	puts "Another name?"
-	name2 = gets.chomp
-if name == "exit"
-	break
-end	
+# Create a loop so that the user can enter in as many names wanted until 
+# they enter done which then exit and print the original name and new name.
+loop do 
+	puts "What is your name? (or type 'done')"
+	user_input = gets.chomp
+	break if user_input == "done"
+	names << user_input
+  end 
 end
 
+
+# Got stucked but wanted to turn in something rather then nothing. 
