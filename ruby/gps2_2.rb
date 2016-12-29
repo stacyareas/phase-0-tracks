d6349cceff4bea44484145ddfa685736be1380ed
Stacy Areas <stacyareas@gmail.com>
2016-12-29 15:44:45 -0800

    psudeocode for grocery list

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,37 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # add items to your list
+  # set default quantity
+  # print the list to the console 
+  # [can you use one of your other methods here?]
+# output: list to be stored in a hash with the item name and quantity of that item
+
+# Method to add an item to a list
+# input: list, item name, and optional quantity
+# steps:
+  # add items to the already existing list
+  # give new items quantity
+  # print the new list 
+# output: updated list with new items and quantity that will be stored in already existing hash
+
+# Method to remove an item from the list
+# input: taking away items and quantity from the existing list
+# steps:
+  # enter items that are wanting to be removed 
+  # print new list with removed items
+# output: updated list with removed items and quantity that will be stored in already existing hash
+
+# Method to update the quantity of an item
+# input: quantity
+# steps: 
+  # update quantity for specific items 
+  # print list
+# output: updated list of the updated quantity that will be stored in already existing hash
+
+# Method to print a list and make it look pretty
+# input: list, item name, and optional quantity
+# steps:
+  # orginized items and quantity
+  # set up output to look nice 
+# output: grocery list that look nice
