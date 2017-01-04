a2398a0dc6d5df645773e7d3bd49e3086858fec6
Stacy Areas <stacyareas@gmail.com>
2017-01-03 16:45:23 -0800

    added new pseudocode

--- js/explore.js
+++ js/explore.js
@@ -1,11 +1,15 @@
 // Create a function that takes a parameter
-// Set 
+// Set variable to equal an empty -""- which splits into characters
+// For each character we want to store into new variable and put 
+// the characters in reverse order then join characters together
+// returning our new string
+
 
 function reverse(s) {
-    var string = "";
+    var string = " ";
     for(var i = s.length - 1; i >= 0; i--)
     string += s[i];
     return string;
 }
 
-console.log(reverse("Hello!"));
+console.log(reverse("Cat in the hat"));
