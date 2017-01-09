9d07ed9b6962d5800e77f0bbb4bb964ac5110440
Stacy Areas <stacyareas@gmail.com>
2017-01-08 20:53:51 -0800

    added peusdocode to  my class

--- Practice_Assessment/stacy_s_areas/spaceship.rb
+++ Practice_Assessment/stacy_s_areas/spaceship.rb
@@ -1,27 +1,29 @@
 class Spaceship
-attr_accessor :name
+attr_accessor :name, :location
 attr_reader :speed
+
 
     def initialize(name, speed)
         @name = name
         @speed = 200000
-        @location = location
     end
-
+# Adding a method that have a name and speed the Spaceship
     def name_and_speed
         puts "Name is: #{@name} and has a speed of: #{@speed}" 
     end
-
+# When endable is called it will print that it has protection
     def enable_sheild
        puts "You now have protection."
     end
-   
+# When disable is called it will print it doesn't have protection   
     def disable_sheild
         puts "You don't have protection."
     end
-
-    def wrap_to(location)
+# warp_to method takes a parameter of location and print 
+# a statement saying how fast it is going and to where. 
+    def warp_to(location)
         puts "Traveling at #{@speed} to #{location}!"
+        collected_items
     end 
 end
 
@@ -30,5 +32,5 @@ spaceship.name_and_speed
 
 spaceship = Spaceship.new("Molly", @speed)
 spaceship.enable_sheild
-spaceship.wrap_to("Mars")
-spaceship.wrap_to("Earth")
+spaceship.warp_to("Mars")
+spaceship.warp_to("Earth")
