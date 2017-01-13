11e3f93cb1778a787fc01c5b86f8c57161646bb3
Stacy Areas <stacyareas@gmail.com>
2017-01-13 00:54:30 -0800

    program that prompts for user input and store in a hash

---
+++ ruby/hashes_foxes.rb
@@ -0,0 +1,33 @@
+clients = {}
+
+puts "What is your name?"
+clients[:name] = gets.chomp
+
+puts "How old are you?"
+clients[:age] = gets.chomp.to_i
+
+puts "How many children do you have?"
+clients[:num_of_kids] = gets.chomp.to_i
+
+puts "What color scheme would you like?"
+clients[:color] = gets.chomp
+
+p clients
+
+puts "What you like to update any infomation?"
+response = gets.chomp
+
+if response == "yes"
+	puts "What would you like to update?"
+	update = gets.chomp.to_sym
+	puts "What would you like to change this to?"
+	change = gets.chomp
+	clients[update] = change
+	p clients
+else 
+	p clients
+end 
+
+
+
+
