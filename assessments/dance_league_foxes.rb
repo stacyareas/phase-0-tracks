b5f011145dd0bd6cd7b59cf115598a43d0ab8892
Stacy Areas <stacyareas@gmail.com>
2017-01-17 21:46:36 -0800

    dance league class and rspec

---
+++ assessments/dance_league_foxes.rb
@@ -0,0 +1,40 @@
+class Dancer
+    attr_reader :name 
+    attr_accessor :age, :card, :new_partner
+
+    def initialize (name, age)
+        @name = name 
+        @age = age
+        @card = []
+        @new_partner = new_partner
+    end
+
+    def pirouette
+        "*twirls*"
+    end    
+
+    def bow
+        "*bows*"
+    end
+
+    def queue_dance_with(partner)
+        @card << partner
+    end 
+
+    def begin_next_dance
+        @new_partner = @card.shift
+        "Now dancing with #{@new_partner}."
+    end
+## 
+    def tutu_color
+        colors = ["blue", "white", "green","pink", "orange", "yellow"]
+        colors.sample
+    end    
+end 
+
+dancer = Dancer.new("Stacy Areas", 21)
+p dancer.queue_dance_with("Lamar Poole")
+p dancer.queue_dance_with("Stacy Areas")
+p dancer.begin_next_dance
+p dancer.begin_next_dance
+p dancer.tutu_color
