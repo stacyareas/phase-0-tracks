6a8a50c65f8c137bd7cdb2563ba6790d0a028b5b
Stacy Areas <stacyareas@gmail.com>
2017-01-20 19:13:31 -0800

    created a mixin module

--- ruby/shout_foxes.rb
+++ ruby/shout_foxes.rb
@@ -1,4 +1,35 @@
-module Shout
+#module Shout
+
+#	def self.yell_angrily(words)
+#		words + "!!!" + " :["
+#	end
+
+#	def self.yelling_happily(words)
+#		words + "!!!" + " :]"
+#	end
+
+#end 
+
+#p Shout.yelling_happily("I love you")
+#p Shout.yell_angrily("I hate you")
 
 
+module Shout 
+	def shouting_things(food_type)
+		puts "Give me #{food_type}! I AM HUNGRY!!!!"
 end 
+end
+
+	class Cats
+		include Shout
+	end
+
+	class Dogs
+		include Shout
+	end
+
+cats = Cats.new
+cats.shouting_things("tuna treats")	
+
+dogs = Dogs.new
+dogs.shouting_things("bacon bits")
