25a8455997915f71cdd1f2b456420519a5e0f19b
Stacy Areas <stacyareas@gmail.com>
2017-01-25 23:00:22 -0800

    added keyValue match fucntion

--- js/algos_foxes.js
+++ js/algos_foxes.js
@@ -8,23 +8,35 @@ var phrases = ["the cat", "in the hats", "likes rats"]
 			longestPhrase = phrases[i];
 	}
 }
-console.log(longestPhrase)
+console.log(longestPhrase);
 
 var personOne = {'age' : 27, 'name' : 'stacy'};
-var personTwo = {'age' : 29, 'name' : 'lamar'};
+var personTwo = {'age' : 27, 'name' : 'lamar'};
 
-function checkIfObjectsMatch(one, two) {
-	for (var i in one) {
-		if (one[i] = two[i] || one[i] != two[i]) {
-			return true;
-		}
-	}
-	return false;
+function checkIfObjectContains(one, two){
+   for (var key in one) {
+     for (var secondKey in two) {
+     		if (key === secondKey) {
+     			if (one[key] === two[secondKey]) {
+     				return true;
+     			}
+     		}
+     	}
+    }
+    return false;
+  }
+
+  console.log(checkIfObjectContains(personOne, personTwo));
+
+  function makeid()
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
 }
 
-console.log(checkIfObjectsMatch(personOne, personTwo))
-
-
-
-
-
+console.log(makeid());
